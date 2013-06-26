6a9087e680453928ea1d8357aa8c9f70fd0ccb5e
oprbguitar <oprbguitar@gmail.com>
2013-06-26 01:41:20 -0500

    desing

---
+++ desing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Desing::Application.config.secret_token = '5a3f40e8edba5b87e64dd2176d652aa8cbcadf2a1584b6cd4f2a9fd7a7a6b875372eb989da9ee27221fd24f5945cd05d4c7200e5d02eafb43bdf56ce7945443c'
